fda90e0ff550451fef5fbe7a03d7f8e6ce1dcebc
jiminleedev0220 <jiminleedev0220@gmail.com>
2022-01-05 03:16:02 +0900

    Revert "Linting"
    This reverts commit 5376576ad26c175e018c71f36768784ca2b017fd.

--- Main.cs
+++ Main.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Drawing;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BinarySphere
@@ -19,42 +18,10 @@ namespace BinarySphere
             Text = "Binary Sphere";
 
             var core = new Core();
-            var a = new a();
-            var buf = new BufferedGraphicsContext().Allocate(CreateGraphics(), ClientRectangle);
 
-            core.SetFPS(100);
+            core.Routine= new Action(()=>{
 
-            core.Routine = new Action(() =>
-            {
-                buf.Graphics.Clear(Color.White);
-                buf.Graphics.DrawImage(a.Draw(core.FPS), new Point(a.Location.X, a.Location.Y));
-                buf.Render();
             });
-
-            core.Initialize();
-
-            FormClosing += async (o, e) =>
-            {
-                if (core.IsTerminated)
-                {
-                    return;
-                }
-
-                e.Cancel = true;
-
-                core.Terminate();
-
-                while (!core.IsTerminated)
-                {
-                    await Task.Delay(1);
-                }
-
-                e.Cancel = false;
-
-                Close();
-            };
-
-            core.Run();
         }
     }
 }
